42448863653bcedc28692f7c30a71a4269d4fd07
rushiikeshshinde <rushiikeshshiinde@gmail.com>
2024-09-26 11:35:04 +0530

    Added: UC1: Calculated Fare

--- src/main/java/org/example/CabInvoiceGenerator.java
+++ src/main/java/org/example/CabInvoiceGenerator.java
@@ -2,4 +2,11 @@ package org.example;
 
 public class CabInvoiceGenerator {
 
+    private static final double COST_PER_TIME = 1;
+    private static final double MINIMUM_COST_PER_KM = 10;
+
+    double calculateFare(double distance, double fare){
+       return (MINIMUM_COST_PER_KM*distance)+(COST_PER_TIME*fare);
+    }
+
 }
--- src/test/java/org/example/CabInvoiceGeneratorTest.java
+++ src/test/java/org/example/CabInvoiceGeneratorTest.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class CabInvoiceGeneratorTest {
+public class CabInvoiceGeneratorTest {
+
+    @Test
+    public void givenDistanceAndTimeShouldReturnFare(){
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        double distance = 2;
+        double time = 5;
+        double fare = cabInvoiceGenerator.calculateFare(distance, time);
+        assertEquals(25,fare);
+    }
 
 }
